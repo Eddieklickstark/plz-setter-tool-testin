056a6f66e26dc295ae597c9295a2d06b7fff0f40
Eddieklickstark <eddie.esche@digital-act.de>
2025-02-10 10:35:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -96,7 +96,60 @@
                         <input type="text" class="ios-input required" name="firma" placeholder="Firma*" required>
                         <select class="ios-input required" name="branche" required>
                             <option value="">Branche wählen*</option>
-                            <!-- Liste der Branchen hier... -->
+                            <option value="MSP">MSP (Management-Dienstleistungsanbieter)</option>
+                            <option value="another">Another option</option>
+                            <option value="URP">URP (Unternehmensressourcenplanung)</option>
+                            <option value="Regierung">Regierung/Militär</option>
+                            <option value="Speicherungs-Dienstleistungsanbieter">Speicherungs-Dienstleistungsanbieter</option>
+                            <option value="Dienstleistungsanbieter">Dienstleistungsanbieter</option>
+                            <option value="Netzwerkausrüstungsunternehmen">Netzwerkausrüstungsunternehmen</option>
+                            <option value="Grossunternehmen">Großunternehmen</option>
+                            <option value="ASA">ASA (Applikationsserviceanbieter)</option>
+                            <option value="Systemintegrator">Systemintegrator</option>
+                            <option value="Klein_Mittelstaendige">Klein/Mittelständige Unternehmen</option>
+                            <option value="Nicht_Management_ISV">Nicht-Management-ISV</option>
+                            <option value="Management_ISV">Management ISV</option>
+                            <option value="Daten_Telekom_OEM">Daten/Telekom-OEM</option>
+                            <option value="Glashersteller">Glashersteller</option>
+                            <option value="Investmentfirma">Investmentfirma</option>
+                            <option value="Sporthalle">Sporthalle</option>
+                            <option value="Privatperson">Privatperson</option>
+                            <option value="Stadien">Stadien</option>
+                            <option value="Brauerei">Brauerei</option>
+                            <option value="Isoliertechnik">Isoliertechnik</option>
+                            <option value="Vermoegensverwaltung">Vermögensverwaltung</option>
+                            <option value="Spedition">Spedition</option>
+                            <option value="Bauprojektentwickler">Bauprojektentwickler</option>
+                            <option value="Textilindustrie">Textilindustrie</option>
+                            <option value="Maschinenbauunternehmen">Maschinenbauunternehmen</option>
+                            <option value="Metallindustrie">Metallindustrie</option>
+                            <option value="Immobilien">Immobilien</option>
+                            <option value="Elektroindustrie">Elektroindustrie</option>
+                            <option value="Dienstleistungen">Dienstleistungen</option>
+                            <option value="Lebensmittelindustrie">Lebensmittelindustrie</option>
+                            <option value="Logistik_Fulfillment">Logistik/Fulfillment</option>
+                            <option value="Rechenzentren">Rechenzentren</option>
+                            <option value="MedTech">MedTech</option>
+                            <option value="Entsorger">Entsorger</option>
+                            <option value="Automobilindustrie">Automobilindustrie</option>
+                            <option value="Moebelindustrie">Möbelindustrie</option>
+                            <option value="Gewerbeflaechen">Gewerbeflächen</option>
+                            <option value="Elektroinstallation">Elektroinstallation</option>
+                            <option value="Verpackungstechnik">Verpackungstechnik</option>
+                            <option value="Recyclingtechnik">Recyclingtechnik</option>
+                            <option value="Farben_Lackbranche">Farben- und Lackbranche</option>
+                            <option value="Hersteller_von_Batterien">Hersteller von Batterien</option>
+                            <option value="Landwirtschaft">Landwirtschaft</option>
+                            <option value="Kunststoffindustrie">Kunststoffindustrie</option>
+                            <option value="Papierindustrie">Papierindustrie</option>
+                            <option value="Grosshandel">Großhandel</option>
+                            <option value="Druckerei">Druckerei</option>
+                            <option value="Behoerde">Behörde</option>
+                            <option value="Frachtspeditionsdienst">Frachtspeditionsdienst</option>
+                            <option value="Lackindustrie">Lackindustrie</option>
+                            <option value="Elektrogeraete_Hersteller">Elektrogeräte Hersteller</option>
+                            <option value="Speicheraufruestung">Speicheraufrüstung</option>
+                            <option value="Optische_Netze">Optische Netze</option>
                         </select>
                     </div>
                 </div>
